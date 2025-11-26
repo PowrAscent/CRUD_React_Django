a20a64476ef9e8596b311f5517839109bd5e324f
orbie3d <orbie3d@gmail.com>
2025-11-25 22:35:20 -0300

    fix: formato de fecha en carga de datos arreglado

--- Django/empleados_django/empleados/management/commands/carga_datos.py
+++ Django/empleados_django/empleados/management/commands/carga_datos.py
@@ -4,8 +4,8 @@ from empleados.models import Empleados
 class Command(BaseCommand):
     def handle(self, *args, **options):
         lista_empleados = [
-            Empleados(nombre='Sammy', apellido='Reyes', puesto='Desarrollador', salario=1500000, fecha_ingreso='23-05-2025', activo=True, telefono='972123485', direccion='La Paz', departamento='Logistica', email='sammy@gmail.com'),
-            Empleados(nombre='Luciano', apellido='Andrades', puesto='Diseñador', salario=800000, fecha_ingreso='12-09-2025', activo=True, telefono='990214566', direccion='Don Mateo 123', departamento='Publicidad', email='luciano@gmail.com'),
+            Empleados(nombre='Sammy', apellido='Reyes', puesto='Desarrollador', salario=1500000, fecha_ingreso='2025-12-22', activo=True, telefono='972123485', direccion='La Paz', departamento='Logistica', email='sammy@gmail.com'),
+            Empleados(nombre='Luciano', apellido='Andrades', puesto='Diseñador', salario=800000, fecha_ingreso='2025-08-24', activo=True, telefono='990214566', direccion='Don Mateo 123', departamento='Publicidad', email='luciano@gmail.com'),
         ]
         Empleados.objects.bulk_create(lista_empleados)
         self.stdout.write('Clientes registrados correctamente!')
