3616a70351ab639253202107a62553fc6f36acaa
Franco_SJ <jean.jara.fs@gmail.com>
2025-11-25 23:59:45 -0300

    feat: listar empleados

--- Django/empleados_django/empleados/views.py
+++ Django/empleados_django/empleados/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+from empleados.models import Empleados
+from empleados.serializers import EmpleadoSerializer
 
+
+class EmpleadosGetPost(APIView):
+
+    def get(self, request):
+        empleados = Empleados.objects.all()
+        serializer = EmpleadoSerializer(empleados, many=True)
+        return Response(serializer.data)
